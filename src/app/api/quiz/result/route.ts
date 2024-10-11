f857282e39ee201b458702f6137ab214e4c416b5
weisintai <wstai1214@gmail.com>
2024-10-11 16:24:31 +0800

    Refactor route.ts to remove unused import and add dynamic export

--- src/app/api/quiz/result/route.ts
+++ src/app/api/quiz/result/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import { drugPreventionSurvey } from "@/db/schema";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const surveyResults = await db.select().from(drugPreventionSurvey);
